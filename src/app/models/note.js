b14b504c9ba44b18be9e4bf67b4ce635e09ef37f
Thomas Belhacel <thomas.belhacel@gmail.com>
2016-11-29 11:12:46 +0100

    Correction note.js

--- src/app/models/note.js
+++ src/app/models/note.js
@@ -7,7 +7,7 @@ var noteSchema = mongoose.Schema({
 	local			: {
 		id_user 	: int,
 		id_movie	: int,
-		criteres	: {int, int, int, int, int, int, int, int, int, int, int, int, int}
+		criteres	: {"Scénario": int, "Jeu d’acteurs": int, "Réalisation": int, "Bande son": int, "Ambiance": int, "Lumière": int, "Montage": int, "Dialogues": int, "Décors": int, "Costumes": int, "Narration": int, "Rythme": int, "SFX" :int}
 	}
 	
 });
